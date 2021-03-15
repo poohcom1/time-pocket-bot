c838dea4ec016bfb7999b8a8d92ef48753f707bf
poohcom1 <pooh677@gmail.com>
2021-03-15 11:47:24 +0700

    Added schedule conversion functions

--- schedule_manager.py
+++ schedule_manager.py
@@ -1,26 +1,29 @@
-# Schedule array
-TIME_SLOTS = 7 * 24 * 4
 
-# Axis 1: Time slots, # Axis 2: Event_id (username), # Axis 3: Event meta data
-master_schedule = [[[0 for k in range(0)] for j in range(0)] for i in range(TIME_SLOTS)]
+# Days
+WEEK_DAYS = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday"
+}
+
+def generate_shedule() -> list:
+    TIME_SLOTS = 7 * 24 * 4
+    return [[[0 for k in range(0)] for j in range(0)] for i in range(TIME_SLOTS)]
 
 
 def add_event(schedule: list, start_time: int, end_time: int, user_id: str, priority: int, name: str):
-    """
-    Adds event to array
-    """
+    """Adds event to array"""
     event = [user_id, priority, name]
     for i in range(start_time, end_time):
         schedule[i].append(event)
 
 
 def find_user_event(schedule: list, user_id: str) -> list:
-    """
-    Returns a list of all events of the given user
-    :param schedule:
-    :param user_id:
-    :return:
-    """
+    """Returns a list of all events of the given user"""
     user_events = []
     for event in schedule:
         if event[0] == user_id:
@@ -28,7 +31,25 @@ def find_user_event(schedule: list, user_id: str) -> list:
     return user_events
 
 
-def timeslot_to_time(timeslot: int) -> [int, int]:
-    hour_quadrant = timeslot % 24*4
-    minute_quadrant = timeslot % 4
-    return hour_quadrant, minute_quadrant
+# ============================================ CONVERSIONS =================================
+def time_index_to_string(index: int) -> str:
+    if index < 0 or index > 24 * 4 * 7:
+        return "Time index out of range"
+
+    index %=  24 * 4 
+    
+    hour = index // 4
+    quarter = index % 4 * 15
+
+    return str(hour) + ":" + f"{quarter:02d}"
+
+
+def time_index_to_day(index: int) -> str:
+    if index < 0 or index > 24 * 4 * 7:
+        return "Time index out of range"
+
+    index //= 24*4
+
+    return WEEK_DAYS[index]
+
+
